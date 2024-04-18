80d6d915fbaf067fec7aa257df7adc1d369d6cbf
jedidahkathure <kathurejedidah37@gmail.com>
2024-04-18 09:49:22 +0300

    save and push

--- src/main/java/com/example/simplebankingsystem/Config/SecurityConfig.java
+++ src/main/java/com/example/simplebankingsystem/Config/SecurityConfig.java
@@ -1,2 +1,28 @@
-package com.example.simplebankingsystem.Config;public class SecurityConfig {
+package com.example.simplebankingsystem.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/icons/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
 }
